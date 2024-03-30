d40078c6c8e7c6b4690276cbce5a85a92470bd7f
Cédric <cedricpopille@outlook.fr>
2024-03-30 03:23:17 +0100

    t

--- src/game/Partie.cpp
+++ src/game/Partie.cpp
@@ -7,68 +7,76 @@ Partie::Partie(sf::RenderWindow * window, Player * creator) :  _window(window), 
     _creator = true;
     _playerList = new std::vector<std::pair<Player*, Personnage*>>();
     
-    if(_player->joinAGame())
-        _player->signalArrival(static_cast<sf::Uint8>(0),static_cast<sf::Uint8>(2));
-    else std::cout << "bug"<< std::endl;
-    
-    sf::Packet packet;
-    if(_player->getSocket()->receive(packet) == sf::Socket::Done)
+    if(!_player->joinAGame())
     {
-        std::cout << "paquet recu : ";
-        if(packet >> _typeOfPacket)
-        {
-            std::cout << static_cast<int>(_typeOfPacket)<< " --> ";
-            if(_typeOfPacket == static_cast<sf::Uint8>(1))
-            {
-                _player->getSocket()->setBlocking(false);
-                packet >> _numberOfPlayer;
-                std::cout << static_cast<int>(_numberOfPlayer) <<std::endl;
-                _lobby = new Lobby(_window, _player, _numberOfPlayer);
-                if((_exit = _lobby->Run()))
-                {
-                    _exitToMenu = _lobby->getExitToMenu();   
-                    return;
-                }
-            }
-            else if(_typeOfPacket == static_cast<sf::Uint8>(6) || _typeOfPacket == static_cast<sf::Uint8>(7))
-            {
-                //!Afficher un message d'erreur au client
-                if(_typeOfPacket == static_cast<sf::Uint8>(7))
-                {
-                    packet >> _errorMessage;
-                    std::cout << "erreur : " << _errorMessage << std::endl;
-                }
-                else
-                {
-                    packet >> _playerDisconnected;
-                    std::cout << "deconnexion : " << _playerDisconnected <<std::endl;
-                }
-                
-                _exit = true;
-                _exitToMenu = true;
-            } 
-            else  std::cout <<"Type de paquet non pris en charge" << std::endl;
-        } else std::cout <<"Erreur format du paquet" << std::endl;
-    } else std::cout <<"Serveur inaccessible" << std::endl;;
-    
-    
-    std::cout <<"Nombre de joueur pour la partie : " << static_cast<int>(_numberOfPlayer) << std::endl;;
-  
-    _level = new Monde();
-    _level->initialisation();
-    
-     if (_characterList.size() != _numberOfPlayer)
-    {
-    
-        Personnage::skin skins[] = {Personnage ::toto, Personnage::titi};
-        for (int j = 0; j < _numberOfPlayer; j++)
-        {
-            Personnage *p = new Personnage(_player,_level, skins[j]);
-            _characterList.push_back(p);
-        }
-        _allowingMovement = true;
+        _exit = true;
+        _exitToMenu = true;
+        std::cout << "bug"<< std::endl;
     }
+    else
+    {
+        _player->signalArrival(static_cast<sf::Uint8>(0),static_cast<sf::Uint8>(2));
 
+        
+        sf::Packet packet;
+        if(_player->getSocket()->receive(packet) == sf::Socket::Done)
+        {
+            std::cout << "paquet recu : ";
+            if(packet >> _typeOfPacket)
+            {
+                std::cout << static_cast<int>(_typeOfPacket)<< " --> ";
+                if(_typeOfPacket == static_cast<sf::Uint8>(1))
+                {
+                    _player->getSocket()->setBlocking(false);
+                    packet >> _numberOfPlayer;
+                    std::cout << static_cast<int>(_numberOfPlayer) <<std::endl;
+                    _lobby = new Lobby(_window, _player, _numberOfPlayer);
+                    if((_exit = _lobby->Run()))
+                    {
+                        _exitToMenu = _lobby->getExitToMenu();   
+                        return;
+                    }
+                }
+                else if(_typeOfPacket == static_cast<sf::Uint8>(6) || _typeOfPacket == static_cast<sf::Uint8>(7))
+                {
+                    //!Afficher un message d'erreur au client
+                    if(_typeOfPacket == static_cast<sf::Uint8>(7))
+                    {
+                        packet >> _errorMessage;
+                        std::cout << "erreur : " << _errorMessage << std::endl;
+                    }
+                    else
+                    {
+                        packet >> _playerDisconnected;
+                        std::cout << "deconnexion : " << _playerDisconnected <<std::endl;
+                    }
+                    
+                    _exit = true;
+                    _exitToMenu = true;
+                } 
+                else  std::cout <<"Type de paquet non pris en charge" << std::endl;
+            } else std::cout <<"Erreur format du paquet" << std::endl;
+        } else std::cout <<"Serveur inaccessible" << std::endl;;
+        
+        
+        std::cout <<"Nombre de joueur pour la partie : " << static_cast<int>(_numberOfPlayer) << std::endl;;
+  
+        _level = new Monde();
+        _level->initialisation();
+        
+         if (_characterList.size() != _numberOfPlayer)
+        {
+        
+            Personnage::skin skins[] = {Personnage ::toto, Personnage::titi};
+            for (int j = 0; j < _numberOfPlayer; j++)
+            {
+                Personnage *p = new Personnage(_player,_level, skins[j]);
+                _characterList.push_back(p);
+            }
+            _allowingMovement = true;
+        }
+
+    }
 }
 
 Partie::Partie(sf::RenderWindow * window, Player * joiner, sf::IpAddress server) :  _window(window), _mutex(new std::mutex)
